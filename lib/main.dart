5fc1698cc44ebf68e56914712d3c15c250f1ebc1
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2025-09-14 12:20:14 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/home_view.dart';
+
+void main () {
+  runApp(const NotesApp());
+}
+
+
+class NotesApp extends StatelessWidget {
+const  NotesApp({super.key}) ;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeView.id : (context) => HomeView() ,
+      } ,
+      initialRoute: HomeView.id,
+    );
+  }
+}

8a2b72d8f91fb62b5bbe5bb213e661dadd9f8903
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2025-09-14 14:28:01 +0300

    create app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,14 +6,22 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
-      body: ListView(
-        children: [
-          Text("Hello "),
-        ],
-      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Notes" , style: TextStyle(fontSize: 30 ),),
+                  Container(width: 50,height: 50, decoration: BoxDecoration(borderRadius: BorderRadius.circular(16) , color: Colors.white.withValues(alpha: .05)), child: IconButton(onPressed: () {}, icon: Icon(Icons.search , size: 35,))),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }

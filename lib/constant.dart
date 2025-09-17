fd89b92b9ea9c595a9bcbc2cc919714854fb5382
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2025-09-17 11:13:04 +0300

    create type adapter and init hive flutter and open box

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Colors.black ;
 const kSecondaryColor = Colors.white ;
 const kThirdColor = Color(0xff62FCD7) ;
+const kNotesBox = "notes_box" ;
--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,8 +1,19 @@
-class NotesModel {
+
+import 'package:hive/hive.dart';
+
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final int color;
+  @HiveField(3)
   final String date;
+
 
   NotesModel({
     required this.title,
@@ -10,5 +21,5 @@ class NotesModel {
     required this.color,
     required this.date,
   });
-  
+
 }

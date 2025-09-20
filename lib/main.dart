60a4cce2b90f56e87fbb1b029a611a3d800b8467
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2025-09-20 08:08:26 +0300

    add modal progress hud and add add notes cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constant.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/notes_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/notes_view.dart';
@@ -18,18 +20,24 @@ class NotesApp extends StatelessWidget {
 const  NotesApp({super.key}) ;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: "Poppins" ,
-        brightness: Brightness.dark,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AddNoteCubit>(create:  (context) => AddNoteCubit() ),
+      ],
+
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: "Poppins" ,
+          brightness: Brightness.dark,
+        ),
+        
+        debugShowCheckedModeBanner: false,
+        routes: {
+          NotesView.id : (context) => NotesView() ,
+          EditNoteView.editViewId : (context) => EditNoteView() ,
+        } ,
+        initialRoute: NotesView.id,
       ),
-      
-      debugShowCheckedModeBanner: false,
-      routes: {
-        NotesView.id : (context) => NotesView() ,
-        EditNoteView.editViewId : (context) => EditNoteView() ,
-      } ,
-      initialRoute: NotesView.id,
     );
   }
 }
